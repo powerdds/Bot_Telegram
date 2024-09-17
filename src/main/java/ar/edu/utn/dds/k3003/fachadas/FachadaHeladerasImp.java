f9579aa38b4ee044e545fc256e9b5c405ed07518
Daniel <danibareiro1996@gmail.com>
2024-09-17 20:41:29 -0300

    Separando fachadas

--- src/main/java/ar/edu/utn/dds/k3003/fachadas/FachadaHeladerasImp.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.utn.dds.k3003.fachadas;
-
-import ar.edu.utn.dds.k3003.complementos.Retiro;
-
-public class FachadaHeladerasImp {
-    public boolean retirar(Retiro retiro) {
-        return true;
-    }
-}
