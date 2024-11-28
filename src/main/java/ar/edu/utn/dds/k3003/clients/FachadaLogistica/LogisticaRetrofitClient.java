1fb15404da7b778dcba7a14769c1c1c5aa5d7a7a
javifgarcia <jagarca@frba.utn.edu.ar>
2024-11-28 16:30:03 -0300

    LogisticaClient inc

---
+++ src/main/java/ar/edu/utn/dds/k3003/clients/FachadaLogistica/LogisticaRetrofitClient.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.dds.k3003.clients.FachadaLogistica;
+
+import ar.edu.utn.dds.k3003.facades.dtos.TrasladoDTO;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+import java.util.List;
+
+public interface LogisticaRetrofitClient {
+    @GET("traslados/search/findByColaboradorId")//query
+    Call<List<TrasladoDTO>> get(@Query("colaboradorId") Long colaboradorId);
+}
